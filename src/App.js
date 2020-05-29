d5795ba571ce90dafba0355ceb6269f546dd813f
SkullKingAbel <abelardo.rivas1@gmail.com>
2020-05-29 17:34:27 -0400

    manipuladno el estado (state)

--- src/App.js
+++ src/App.js
@@ -10,11 +10,20 @@ class App extends Component {
     ]
   }
 
+  cambiarNombreHandler = () => {
+    this.setState({
+      array_de_personas: [
+        { nombre: 'Abel', edad: 22 },
+        { nombre: 'Manuel', edad: 23}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hola, soy una aplicación de React</h1>
-        <button>Cambiar Nombre</button>
+        <button onClick={this.cambiarNombreHandler}>Cambiar Nombre</button>
         <Persona nombre={this.state.array_de_personas[0].nombre} edad={this.state.array_de_personas[0].edad} >Mis hobbies: Correr</Persona>
         <Persona nombre={this.state.array_de_personas[1].nombre} edad={this.state.array_de_personas[1].edad} />
       </div>
