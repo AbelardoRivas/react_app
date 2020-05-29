b04d31eaf1f0fffa07180f03ffc6752296663e2d
SkullKingAbel <abelardo.rivas1@gmail.com>
2020-05-29 19:00:08 -0400

    manejando el estado con React Hooks (useState)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import Persona from './Persona/Persona';
 
-class App extends Component {
-  state = {
+const app = propiedades => {
+
+  const [estadoPersonas, cambiarEstadoPersonas] = useState({
     array_de_personas: [
       { nombre: 'Abelardo', edad: 23 },
       { nombre: 'Manuel', edad: 24 }
     ]
-  }
+  })
 
-  cambiarNombreHandler = () => {
-    this.setState({
+  const cambiarNombreHandler = () => {
+    cambiarEstadoPersonas({
       array_de_personas: [
         { nombre: 'Abel', edad: 22 },
-        { nombre: 'Manuel', edad: 23}
+        { nombre: 'Manuel', edad: 23 }
       ]
     })
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hola, soy una aplicación de React</h1>
-        <button onClick={this.cambiarNombreHandler}>Cambiar Nombre</button>
-        <Persona nombre={this.state.array_de_personas[0].nombre} edad={this.state.array_de_personas[0].edad} >Mis hobbies: Correr</Persona>
-        <Persona nombre={this.state.array_de_personas[1].nombre} edad={this.state.array_de_personas[1].edad} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hola, soy una aplicación de React</h1>
+      <button onClick={cambiarNombreHandler}>Cambiar Nombre</button>
+      <Persona
+        nombre={estadoPersonas.array_de_personas[0].nombre}
+        edad={estadoPersonas.array_de_personas[0].edad} >Mis hobbies: Correr
+        </Persona>
+      <Persona
+        nombre={estadoPersonas.array_de_personas[1].nombre}
+        edad={estadoPersonas.array_de_personas[1].edad}
+      />
+    </div>
+  );
 }
 
-export default App;
+export default app;
