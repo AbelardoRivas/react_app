e04f12d13c14bd0b13758da2566d6e7cfbede9ab
SkullKingAbel <abelardo.rivas1@gmail.com>
2020-05-29 17:16:24 -0400

    manejando el estado (state)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import Persona from './Persona/Persona';
 
 class App extends Component {
+  state = {
+    array_de_personas: [
+      { nombre: 'Abelardo', edad: 23 },
+      { nombre: 'Manuel', edad: 24 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hola, soy una aplicación de React</h1>
-        <Persona nombre="Abel" edad="22" >Mis hobbies: Correr</Persona>
-        <Persona nombre="Manu" edad="23" />
+        <button>Cambiar Nombre</button>
+        <Persona nombre={this.state.array_de_personas[0].nombre} edad={this.state.array_de_personas[0].edad} >Mis hobbies: Correr</Persona>
+        <Persona nombre={this.state.array_de_personas[1].nombre} edad={this.state.array_de_personas[1].edad} />
       </div>
     );
   }
