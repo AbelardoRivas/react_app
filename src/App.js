1c0dfaca4706afbf26f72bf8785cfaaff4a80ef0
SkullKingAbel <abelardo.rivas1@gmail.com>
2020-05-29 20:18:18 -0400

    pasando referencias a métodos entre componentes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Persona from './Persona/Persona';
 
-const app = propiedades => {
-
-  const [estadoPersonas, cambiarEstadoPersonas] = useState({
+class App extends Component {
+  state = {
     array_de_personas: [
       { nombre: 'Abelardo', edad: 23 },
       { nombre: 'Manuel', edad: 24 }
     ]
-  })
+  }
 
-  const cambiarNombreHandler = () => {
-    cambiarEstadoPersonas({
+  cambiarValorHandler = (nuevoNombre) => {
+    this.setState({
       array_de_personas: [
-        { nombre: 'Abel', edad: 22 },
-        { nombre: 'Manuel', edad: 23 }
+        { nombre: nuevoNombre, edad: 22 },
+        { nombre: 'Manuel', edad: 23}
       ]
     })
   }
 
-  return (
-    <div className="App">
-      <h1>Hola, soy una aplicación de React</h1>
-      <button onClick={cambiarNombreHandler}>Cambiar Nombre</button>
-      <Persona
-        nombre={estadoPersonas.array_de_personas[0].nombre}
-        edad={estadoPersonas.array_de_personas[0].edad} >Mis hobbies: Correr
+  render() {
+    return (
+      <div className="App">
+        <h1>Hola, soy una aplicación de React</h1>
+        <button onClick={this.cambiarValorHandler.bind(this, '¡Abelardo!')}>Cambiar Nombre</button>
+        <Persona 
+          nombre={this.state.array_de_personas[0].nombre} 
+          edad={this.state.array_de_personas[0].edad}
+          click={this.cambiarValorHandler} >Mis hobbies: Correr
         </Persona>
-      <Persona
-        nombre={estadoPersonas.array_de_personas[1].nombre}
-        edad={estadoPersonas.array_de_personas[1].edad}
-      />
-    </div>
-  );
+        <Persona 
+          nombre={this.state.array_de_personas[1].nombre} 
+          edad={this.state.array_de_personas[1].edad} 
+        />
+      </div>
+    );
+  }
 }
 
-export default app;
+export default App;
--- src/Persona/Persona.js
+++ src/Persona/Persona.js
@@ -3,7 +3,7 @@ import React from 'react';
 const persona = (propiedades) => {
     return (
         <div>
-            <p>Hola, soy {propiedades.nombre} persona y tengo {propiedades.edad} años de edad</p>
+            <p onClick={propiedades.click}>Hola, soy {propiedades.nombre} persona y tengo {propiedades.edad} años de edad</p>
             <p> {propiedades.children} </p>
         </div>
     )
